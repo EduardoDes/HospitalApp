4235be850809a9836e4dd347a6d6a6941d90355c
Eduardo Maturana <ematuranacabrera@gmail.com>
2021-06-11 16:07:39 -0400

    Control del menu

---
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { SettingsService } from '../../services/settings.service';
+
+@Component({
+  selector: 'app-account-settings',
+  templateUrl: './account-settings.component.html',
+  styles: [
+  ]
+})
+export class AccountSettingsComponent implements OnInit {
+
+
+
+  constructor(private settingsService : SettingsService) { }
+
+  ngOnInit(): void {
+
+    this.settingsService.checkCurrentTheme();
+    
+  }
+
+  changeTheme(theme : string){
+
+ 
+    this.settingsService.changeTheme(theme);
+
+  }
+
+  
+}
